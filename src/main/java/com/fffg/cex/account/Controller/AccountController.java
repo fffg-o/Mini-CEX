9ccb9cdda4e7f940aa6e27837d4f4799d12fe40b
fffg-o <sunzhiouyang@gmail.com>
2026-05-10 14:52:36 +0800

    fix bug requestmapping

--- src/main/java/com/fffg/cex/account/Controller/AccountController.java
+++ src/main/java/com/fffg/cex/account/Controller/AccountController.java
@@ -7,10 +7,12 @@ import com.fffg.cex.common.result.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/accounts")
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
 
     @Autowired
--- src/main/java/com/fffg/cex/market/Controller/MarketController.java
+++ src/main/java/com/fffg/cex/market/Controller/MarketController.java
@@ -7,11 +7,13 @@ import com.fffg.cex.market.VO.AssetVO;
 import com.fffg.cex.market.VO.SymbolPairVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/markets")
+@RestController
+@RequestMapping("/markets")
 public class MarketController {
 
     @Autowired
